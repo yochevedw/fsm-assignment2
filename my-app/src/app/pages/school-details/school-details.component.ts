db9d215fc6aae4f12d18c312636585b37c69e786
yochevedw <65480858+yochevedw@users.noreply.github.com>
2021-12-06 11:09:41 +0200

    Add files via upload

---
+++ my-app/src/app/pages/school-details/school-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CandidateModel } from 'src/app/Models/candidateModel';
+
+@Component({
+  selector: 'app-school-details',
+  templateUrl: './school-details.component.html',
+  styleUrls: ['./school-details.component.scss']
+})
+export class SchoolDetailsComponent implements OnInit {
+
+  @Input() candidate: CandidateModel = new CandidateModel();
+  @Output() moveToNextState: EventEmitter<any> = new EventEmitter();
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  btnNextClicked()
+  {
+      this.moveToNextState.emit('Next_Pressed');
+  }
+
+  btnBackClicked()
+  {
+      this.moveToNextState.emit('Back_Pressed');
+  }
+
+}
