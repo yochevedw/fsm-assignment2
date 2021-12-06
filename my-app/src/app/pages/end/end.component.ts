cec4c998519266ee445a01e2e06f6cac3796fb89
yochevedw <65480858+yochevedw@users.noreply.github.com>
2021-12-06 11:08:33 +0200

    Add files via upload

---
+++ my-app/src/app/pages/end/end.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CandidateModel } from 'src/app/Models/candidateModel';
+import { StateEnum } from 'src/app/Utils/enums';
+
+@Component({
+  selector: 'app-end',
+  templateUrl: './end.component.html',
+  styleUrls: ['./end.component.scss']
+})
+export class EndComponent implements OnInit {
+
+  @Input() candidate: CandidateModel = new CandidateModel();
+  @Output() moveToNextState: EventEmitter<any> = new EventEmitter();
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  btnBackClicked()
+  {
+    this.moveToNextState.emit('Back_Pressed');
+  }
+
+}
